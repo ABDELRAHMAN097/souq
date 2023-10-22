9243aa9541da8e0b149b34838f80915d37bfd793
boody <bodymagdy97@gmail.com>
2023-10-22 11:39:58 +0300

    h

---
+++ none.js
@@ -0,0 +1,124 @@
+let disc =20;
+
+switch(disc){
+    default:
+        console.log("unknwen disc");
+        break;
+    case 5 :
+        console.log("gogo");
+        break;
+        case 20:
+            console.log("roqaya");
+
+}
+
+
+
+let price = 200;
+switch(price) {
+    case 200:
+        console.log("mvb");
+        break;
+        case 2020:
+            console.log("boodymagdy097");
+            break;
+            default:
+                console.log("unknwen prise"); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let day = 1;
+switch (day) {
+    case 0:
+    console.log("allah");
+    breack;
+    case 8:
+    console.log("allah-karem");
+    breack;
+    default:
+        console.log("unknwen day");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
